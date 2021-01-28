446b14f2c1f2be9e86fb2e282cf22c2a619fef59
Amichai Berger <amichaiberger14@gmail.com>
2021-01-29 01:45:11 +0200

    almost there

--- java.js
+++ java.js
@@ -2,220 +2,167 @@
 var gridBoxSize = 1600;
 //where game is played
 var containerBox = document.getElementById('container');
-//grid counter
-var i = 1;
 
+createGame();
 
+//current direction
+let direction = ""
+//listning to what direction was pressed
+document.addEventListener("keydown", keydirection)
+//fisrt dot of snake
+var middle = document.getElementById('821');
+middle.style.backgroundColor = 'yellow'
+//saves ransom noumber spot
+var rnd = 1;
+var currentId = middle.id;
+//the snake itself as an array
+var lastpart = [currentId, 1, 1, 1];
+let currentDiv = document.getElementById(currentId)
+
+nextdot();
+//creates the grid of the game
 function createGame() {
-  for (i = 1; i <= gridBoxSize; i++) {
+  for (let i = 1; i <= gridBoxSize; i++) {
     let newCells = document.createElement('div');
     newCells.setAttribute('class', 'gridRows');
     newCells.setAttribute('id', i);
     containerBox.appendChild(newCells);
     //console.log([i])
   }
-
 }
-createGame();
-//current direction
-let direction = ""
-//listning to what direction was pressed
-document.addEventListener("keydown", keydirection)
-let gameAlive = false;
-var middle = document.getElementById('821');
-middle.style.backgroundColor = 'yellow'
-var rnd = 1;
-var currentId = middle.id;
-//console.log(currentId)
-nextdot();
-let currentDiv = document.getElementById(currentId)
-
-
-var lastpart = [currentId, 1, 1];
-
-
-
+//adds one spot to the snake
 function addLength() {
-
   lastpart.push(1);
   score();
 }
-addLength();
-//console.log(lastpart)
-
 function score() {
   let score = lastpart.length - 4;
+  let scoreCard = document.getElementById('scoreId');
+  scoreCard.innerHTML = score;
   console.log(score)
 }
-score();
-
+//makes the last spot of the snake bck to black
 function looper() {
-
   document.getElementById(lastpart[lastpart.length - 1]).style.backgroundColor = "black";
 }
+//if food was eaten creates a new dot and makes the snake bigger
 function foodCollector() {
   if (rnd == currentId) {
     nextdot()
     addLength()
-    // console.log('hello')
   }
-
 }
-
+//creates randomely a tod on the screen
 function nextdot() {
   rnd = Math.floor((Math.random() * 1600 + 1))
   var newfood = document.getElementById(rnd)
   newfood.style.backgroundColor = "white"
-
 }
-
-
-
+//if the snake hits himself
+function hitMyFace() {
+  let las = lastpart.slice(1, lastpart.length);
+  if (las.includes(currentId)) {
+    dead()
+  }
+}
 //figurs out what arrow key was pressed
 function keydirection(e) {
-  let upD;
-  let downD;
-  let rightD;
-  let leftD;
-
   switch (e.keyCode) {
 
     case 37://direction left
       if (direction != "r") {
         direction = "l";
-
-
-
-        leftD = setInterval(function () {
-          // console.log(currentId)
-          if (currentId == 1600) {
-            clearInterval(id);
-            // console.log('game over')
+        let leftD = setInterval(function () {
+          if ((currentId - 1) % 40 == 0) {
+            clearInterval(leftD)
+            dead()
           }
           else {
             if (direction != "l") {
               clearInterval(leftD);
             }
-
-            looper();
-            foodCollector();
-
-
+            codecaller()
             currentId = Number(currentId) - 1;
-            let currentDiv = document.getElementById(currentId)
-            currentDiv.style.backgroundColor = 'yellow'
-            lastpart.unshift(currentId)
-            lastpart.pop();
-            //console.log(lastpart)
-
-
+            workcode()                      
           }
         }, 100);
-
       }
       break;
+
     case 38://direction up
       if (direction != "d") {
         direction = "u";
-        upD = setInterval(function () {
-          if (currentId == 1600) {
-            clearInterval(id);
-            //console.log('game over')
+        let upD = setInterval(function () {
+          if (currentId > 0 && currentId < 40) {
+            clearInterval(upD)
+            dead()
           }
           else {
             if (direction != "u") {
               clearInterval(upD);
             }
-            looper();
-            foodCollector();
-
-
-
+            codecaller()
             currentId = Number(currentId) - 40;
-            let currentDiv = document.getElementById(eval(currentId))
-            currentDiv.style.backgroundColor = 'yellow'
-            lastpart.unshift(currentId)
-            lastpart.pop();
-            // console.log(lastpart)
-
-
-
-
+            workcode()           
           }
         }, 100);
-
       }
       break;
-
 
     case 39://direction right
       if (direction != "l") {
         direction = "r";
-
-        rightD = setInterval(function () {
-          if (currentId == 1 < 40) {
-            clearInterval(id);
-            console.log('game over')
+        let rightD = setInterval(function () {
+          if (currentId % 40 == 0) {
+            clearInterval(rightD)
+            dead()
           }
           else {
             if (direction != "r") {
               clearInterval(rightD);
             }
-            looper();
-            foodCollector();
-
+            codecaller()
             currentId = Number(currentId) + 1;
-            let currentDiv = document.getElementById(eval(currentId))
-            currentDiv.style.backgroundColor = 'yellow'
-            lastpart.unshift(currentId)
-            lastpart.pop();
-            // console.log(lastpart)
-
-
+            workcode()             
           }
         }, 100);
       }
       break;
 
-
-
-
     case 40://direction down
-
       if (direction != "u") {
         direction = "d";
-
-
-        downD = setInterval(function () {
-
-          if (currentId == 0) {
-            clearInterval(id);
-            console.log('game over')
+        let downD = setInterval(function () {
+          if (currentId > 1560 && currentId < 1601) {
+            clearInterval(downD)
+            dead()
           }
           else {
             if (direction != "d") {
               clearInterval(downD);
             }
-            looper();
-            foodCollector();
-
+            codecaller()
             currentId = Number(currentId) + 40;
-            let currentDiv = document.getElementById(eval(currentId))
-            currentDiv.style.backgroundColor = 'yellow'
-            lastpart.unshift(currentId)
-            lastpart.pop();
-            // console.log(lastpart)
-
-
+            workcode()            
           }
         }, 100);
       }
-
       break;
     default:
   }
 }
-function gameover(){
-  if ()
+function workcode(){
+  let currentDiv = document.getElementById(currentId)
+  currentDiv.style.backgroundColor = 'yellow'
+  lastpart.unshift(currentId)
+  lastpart.pop();
 }
-
+function codecaller(){
+  foodCollector();          
+  hitMyFace();
+  looper();           
+}
+function dead(){
+  let loose = document.getElementById("lost")
+  loose.style.display = "block"
+}
